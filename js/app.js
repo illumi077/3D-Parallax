959fe44b3bc7c144e1127e43e2ed982dcd8ecdc9
Shivraj <shivrajkaushik248@gmail.com>
2024-02-11 23:47:31 +0530

    main commited

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const parallax_el = document.querySelectorAll(".parallax")
+
+let xValue = 0
+let yValue = 0
+
+window.addEventListener("mousemove" , (e) => {
+    xValue = e.clientX - window.innerWidth / 2
+    yValue = e.clientY - window.innerHeight / 2
+
+    parallax_el.forEach(elem => {
+        let speedX = elem.dataset.speedx
+        let speedY = elem.dataset.speedy
+        elem.style.transform = `translateX(calc(-50% + ${- xValue * speedX}px ))  translateY(calc(-50% + ${- yValue * speedY}px ))`
+    })
+})
